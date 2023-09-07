8717ed93552762c621f0949126da9d4cbe15aaf4
mishra-sweta <swetamishra.liluah@gmail.com>
2023-09-07 23:54:31 +0530

    create reviews frontend & Pagination

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import { Col, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import Product from "../components/Product.js";
 import Loader from "../components/Loader.js";
 import Message from "../components/Message.js";
+import Paginate from "../components/Paginate.js";
 import { useGetProductsQuery } from "../slices/productsApiSlice.js";
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const {pageNumber} = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({pageNumber});
 
   return (
     <div>
@@ -19,12 +22,15 @@ const HomeScreen = () => {
         <div>
           <h1>Latest Products</h1>
           <Row>
-            {products?.map((product) => (
+            {data.products?.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
           </Row>
+          <Paginate
+          pages={data.pages}
+          page={data.page} />
         </div>
       )}
     </div>
